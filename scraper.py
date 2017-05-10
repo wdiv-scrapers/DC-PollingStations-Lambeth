1d34b808fccbea9345a650f01d8818bffd5e14ea
chris48s <chris.shaw480@gmail.com>
2017-05-10 12:36:52 +0100

    Capture objects with OBJECTID=NULL

--- scraper.py
+++ scraper.py
@@ -1,12 +1,12 @@
 from dc_base_scrapers.arcgis_scraper import ArcGisScraper
 
 
-stations_url = "http://gis.lambeth.gov.uk/arcgis/rest/services/ElectoralServices/MapServer/0/query?where=OBJECTID+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson"
-districts_url = "http://gis.lambeth.gov.uk/arcgis/rest/services/ElectoralServices/MapServer/1/query?where=OBJECTID+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPolygon&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson"
+stations_url = "http://gis.lambeth.gov.uk/arcgis/rest/services/ElectoralServices/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson"
+districts_url = "http://gis.lambeth.gov.uk/arcgis/rest/services/ElectoralServices/MapServer/1/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPolygon&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson"
 council_id = 'E09000022'
 
 
-stations_scraper = ArcGisScraper(stations_url, council_id, 'utf-8', 'stations')
+stations_scraper = ArcGisScraper(stations_url, council_id, 'utf-8', 'stations', key='OBJECTID_1')
 stations_scraper.scrape()
-districts_scraper = ArcGisScraper(districts_url, council_id, 'utf-8', 'districts')
+districts_scraper = ArcGisScraper(districts_url, council_id, 'utf-8', 'districts', key='OBJECTID_1')
 districts_scraper.scrape()
